d378f5e99c547398f9af9ea93960a92afa39b23f
Abdi-01 <abdialghi@gmail.com>
2022-05-30 10:22:56 +0700

    data string

--- day01_intro.js
+++ day01_intro.js
@@ -7,7 +7,7 @@
  * namaVariable : harus diawali dengan huruf atau symbol "_" / symbol "$"
  * 
  * valueVariable : berdasarkan tipe data ==> string, number, boolean, array, object
- * */ 
+ * */
 
 // Keyword var : Redeclare, Reassign
 var data; // deklarasi variable
@@ -17,7 +17,7 @@ nama = "Aku adalah Edo"; // Reassign ✅
 data = "Belajar Programming";
 
 console.log(nama); // console.log(nilai_yg_ingin_ditampilkan)
-console.log(data); 
+console.log(data);
 console.log("Aku Aldo");
 
 // Keyword let : Non-Redeclare, Reassign
@@ -33,3 +33,29 @@ const PI = 3.14;
 // PI = 22/7; // Reassign ❌
 
 console.log(PI);
+
+/////////////////////// ATURAN PENULISAN NAMA VARIABLE //////////////////////
+// 1. Harus diawali dengan huruf
+// let 01data; ❌
+let job; // ✅
+
+// 2. Boleh diawali dengan symbol ( _ atau $ )
+let _phone = '08992828392'; // ✅
+let $fax = "032222"; // ✅
+
+// 3. Variable yang terdiri dari 2 suku kata atau lebih
+let fullName = "Edo Tensei"; // camelCase ✅
+let product_price = 35000; // snake_case ✅
+let alamatkota = "Jakarta"; // ✅
+// let data barang; // ❌
+// let data-barang; // ❌
+
+// Keyboard shortcut yang biasa digunakan
+// shift + alt + f = merapikan program
+// windows + . = memunculkan list icon
+// ctrl + s = untuk save
+// ctrl + / = membuat comment satu baris
+// ctrl + f = mencari program
+// ctrl + ` = membuka terminal
+// shift + alt + ⬇️ atau ⬆️ = untuk menyalin baris program
+// alt +  ⬇️ atau ⬆️ = untuk memindahkan baris program
---
+++ day01_string.js
@@ -0,0 +1,3 @@
+// 1️⃣ String : data berupa huruf atau character
+let namaToko = "Sport Apparel"; // double quote
+let nama_toko = 'Sport Apparel'; // single quote
