130d21d49e2c548271fde97bd22117d66ac0b575
Abdi-01 <abdialghi@gmail.com>
2022-05-30 11:07:49 +0700

    data number

---
+++ day01_number.js
@@ -0,0 +1,37 @@
+// 2️⃣ Number : berkaitan dengan data angka
+
+let jarak = 1500; // Nilai INTEGER/INT
+let berat = 80.5; // Nilai FLOAT/Desimal
+
+// Arithmetic operator : +, -, *, /, %
+let jumlahJarak = jarak + 1000;
+let penguranganJarak = jarak - 1000;
+console.log("Penjumlahan =>", jumlahJarak);
+console.log("Pengurangan =>", penguranganJarak);
+
+// Increment
+jarak++; // 1501
+console.log(jarak)
+jarak++; // 1502
+console.log(jarak)
+jarak++; // 1503
+console.log(jarak)
+
+jarak+=2; // jarak = jarak + 2; // 1505
+console.log(jarak)
+jarak+=2; // jarak = jarak + 2; // 1507
+console.log(jarak)
+
+
+// Decrement
+jarak--;
+console.log(jarak)
+jarak--;
+console.log(jarak)
+jarak--;
+console.log(jarak)
+
+jarak-=2;
+console.log(jarak)
+jarak-=2;
+console.log(jarak)
