c78fd54c39fec95d85014fb1113b991f474229ef
Abdi-01 <abdialghi@gmail.com>
2022-06-02 11:54:41 +0700

    Array

--- day03_array.js
+++ day03_array.js
@@ -24,3 +24,64 @@ console.log('Nama Buah :', namaBuah[2]);
 console.log('Stock Buah :', stockBuah[2]);
 console.log('Harga Buah :', hargaBuah[2]);
 console.log(`Buah ${namaBuah[1]} stoknya ada ${stockBuah[1]} dan harganya Rp. ${hargaBuah[1].toLocaleString('id')}`);
+
+console.table(namaBuah);
+console.table(stockBuah);
+
+// INPUT
+// Variable data buah yg ada di atas
+
+// PROCESS
+let listBuah = '';
+for (let i = 0; i < namaBuah.length; i++) {
+    listBuah += `${i + 1}. Buah ${namaBuah[i]} stoknya ada ${stockBuah[i]} dan harganya Rp. ${hargaBuah[i].toLocaleString('id')};\n`
+}
+
+// OUTPUT
+console.log(listBuah);
+
+//////////////////// ARRAY FUNCTION ///////////////////
+// ⏺️ namaVariableArray.length : untuk mengetahui jumlah data yang ada didalam array
+// OUTPUT dari length bertipe NUMBER
+console.log('Banyak data array namaBuah', namaBuah.length);
+console.log('Mengakses data terakhir sebuah array', namaBuah[namaBuah.length - 1]);
+
+// ⏺️ namaVariableArray.push(data) : digunakan untuk menambah data baru di akhir array
+namaBuah.push('Melon');
+stockBuah.push(7);
+hargaBuah.push(6500);
+console.table(namaBuah);
+console.table(stockBuah);
+console.table(hargaBuah);
+
+// ⏺️ namaVariableArray.pop() : digunakan untuk menghapus data terakhir dari array
+namaBuah.pop();
+stockBuah.pop();
+hargaBuah.pop();
+
+// ⏺️ namaVariableArray.unshift(data) : digunakan untuk menambah data baru di awal array
+namaBuah.unshift('Lemon')
+stockBuah.unshift(20)
+hargaBuah.unshift(3000)
+
+// ⏺️ namaVariableArray.shift() : digunakan untuk menghapus data pertama dari array
+namaBuah.shift()
+stockBuah.shift()
+hargaBuah.shift()
+
+// ⏺️ namaVariable.reverse() : membalikkan urutan data array
+let motor = ['Yamaha', 'Honda', 'Ducati'];
+console.log('Before :', motor);
+motor.reverse();
+console.log('After :', motor);
+
+// ⏺️ namaVariableArray.join("separator") : menggabungkan semua data array menjadi data string
+console.log(motor.join()); // Ducati,Honda,Yamaha
+console.log(motor.join(' ')); // Ducati Honda Yamaha
+console.log(motor.join(' / ')); // Ducati / Honda / Yamaha
+
+// String --> Array
+// namaVariableString.split('Separator');
+let greeting = 'Hello';
+console.log('Before :', greeting);
+console.log('After :', greeting.split());
