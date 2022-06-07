61524605b3ae3b2d25b9bd8764ae02e6a666efcf
Abdi-01 <abdialghi@gmail.com>
2022-06-07 09:31:48 +0700

    array of object

---
+++ Day06_arrayofobject.js
@@ -0,0 +1,26 @@
+// ARRAY OF OBJECT
+
+/**
+ * 1. Merupakan struktur data yang biasa digunakan untuk output dari database
+ * 2. Juga digunakan untuk data JSON 
+ * 3. Struktur penulisan : 
+ *          let arrOfObject = [
+ *              {
+ *                  id:1,
+ *                  name:'Jacket',
+ *                  brand : 'DnD',
+ *                  price : 250000,
+ *                  stock : [
+ *                              {
+ *                                   type:"L",
+ *                                   qty:12
+ *                              },
+ *                              {
+ *                                   type:"XL",
+ *                                   qty:22
+ *                              }
+ *                          ]
+ *              }
+ * 
+ *          ]
+ */
